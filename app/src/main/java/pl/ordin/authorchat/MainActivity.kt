742374b4ee1cc4453fa221189d22d9a1bd97cf82
Piotr Pesta <piotr.pesta@gmail.com>
2019-05-11 15:01:19 +0200

    Gradle configuration.

--- app/src/main/java/pl/ordin/authorchat/MainActivity.kt
+++ app/src/main/java/pl/ordin/authorchat/MainActivity.kt
@@ -1,7 +1,7 @@
 package pl.ordin.authorchat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
