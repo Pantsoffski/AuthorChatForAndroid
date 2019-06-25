1b6be3cc980dab5fdedf7cef706933aef41dd892
Piotr Pesta <piotr.pesta@gmail.com>
2019-06-25 15:56:52 +0200

    MessageItem: + Random color for each nickname.

--- app/src/main/java/pl/ordin/authorchat/main/chat/item/MessageItem.kt
+++ app/src/main/java/pl/ordin/authorchat/main/chat/item/MessageItem.kt
@@ -1,9 +1,12 @@
 package pl.ordin.authorchat.main.chat.item
 
+import android.graphics.Color
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.item_message.*
 import pl.ordin.authorchat.R
+import kotlin.random.Random
+
 
 class MessageItem(
     private val nick: String,
@@ -16,12 +19,21 @@ class MessageItem(
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.apply {
             nickView.text = nick
+            nickView.setTextColor(hexColorGenerator(nick)) // set individual color for each nick
             dateView.text = date
             chatView.text = message
         }
     }
 
     override fun getLayout(): Int = R.layout.item_message
+
+    //generate rgb color (string as a seed)
+    private fun hexColorGenerator(seed: String): Int {
+        val hash = seed.hashCode().toLong()
+        val rnd = Random(hash)
+
+        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+    }
 
     //endregion
 }
